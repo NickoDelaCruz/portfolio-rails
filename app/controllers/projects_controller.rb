cef5d8a661c16515fbe4a0c31bd260e001bfef2c
Nicko DC <nickodc96@gmail.com>
2018-04-29 18:30:07 -0700

    add create and new funct

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,19 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "You've added #{@project.title} to your projects."
+      redirect_to  projects_path
+    else
+      render :new
+    end
+  end
+
 end
