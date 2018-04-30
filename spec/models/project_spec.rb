f4830ebd662c33fa93b9e613f60e8ad06a6fd266
Nicko DC <nickodc96@gmail.com>
2018-04-30 01:46:59 -0700

    typo, specs pass

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Project do
   it { should validate_presence_of :title }
-  it { should validate_presence_of :header }
+  it { should validate_presence_of :desc }
   it { should validate_presence_of :url }
 end
