3e7672dd3060fee31325d5349715b1d1e1e07792
Nicko DC <nickodc96@gmail.com>
2018-04-29 18:09:42 -0700

    add show func in Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
   def index
-      @projects = Project.all.limit(5)
-    end
+    @projects = Project.all.limit(5)
+  end
+  def show
+    @project = Project.find(params[:id])
+  end
 end
