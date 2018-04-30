77f9759686f9ac4b4a6edcc91de719f11df7bd92
Nicko DC <nickodc96@gmail.com>
2018-04-29 21:21:47 -0700

    add scope for top 5 and validations for Projects

---
+++ app/models/project.rb
@@ -0,0 +1,7 @@
+class Project < ActiveRecord::Base
+  validates :title, :presence => true
+  validates :header, :presence => true
+  validates :url, :presence => true
+
+  scope :top_five_most_recent, -> { order(created_at: :desc).limit(5) }
+end
